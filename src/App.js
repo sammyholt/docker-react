270d8019675c37254c94f7855a3b78544e9aac84
Samuel Holt <samuel.l.holt@gmail.com>
2022-11-28 21:57:27 -0800

    Commit to test github action

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there!! Again???? Why!</p>
+        <p>Hello World!</p>
       </header>
     </div>
   );
