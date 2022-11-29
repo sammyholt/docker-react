877b03c78e83e92ff689a3e89928d9af8feee969
Samuel Holt <samuel.l.holt@gmail.com>
2022-11-28 21:08:00 -0800

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders bye there text", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/bye there/i);
   expect(linkElement).toBeInTheDocument();
 });
